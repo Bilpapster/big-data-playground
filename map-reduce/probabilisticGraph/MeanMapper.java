2356842d1540ed75d63adf25e3583e62ba36d25b
Christos Balaktsis <balaktsis@csd.auth.gr>
2024-11-21 14:53:29 +0200

    Cleaned Mapper for prob 4

--- map-reduce/probabilisticGraph/MeanMapper.java
+++ map-reduce/probabilisticGraph/MeanMapper.java
@@ -8,12 +8,9 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class MeanMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
-    private static final Text COUNT_KEY = new Text("count");
-    private static final Text SUM_KEY = new Text("sum");
-
     @Override
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        context.write(COUNT_KEY, new DoubleWritable(1));
-        context.write(SUM_KEY, new DoubleWritable(Double.parseDouble(value.toString().split("\t")[1])));
+        context.write(new Text("count"), new DoubleWritable(1));
+        context.write(new Text("sum"), new DoubleWritable(Double.parseDouble(value.toString().split("\t")[1])));
     }
 }
