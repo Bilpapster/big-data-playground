29f8860fd32f8b104cbdfbc707deaa30ff6cdfdd
Christos Balaktsis <balaktsis@csd.auth.gr>
2025-01-04 20:18:38 +0200

    Refactored output to files

--- spark/src/main/scala/Task2AirlineTweets.scala
+++ spark/src/main/scala/Task2AirlineTweets.scala
@@ -3,9 +3,15 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
 
+import java.io.File
+import scala.reflect.io.Directory
+
 object Task2AirlineTweets {
   def main(args: Array[String]): Unit = {
     Logger.getRootLogger.setLevel(Level.OFF)
+
+    val OUTPUT_DIRECTORY = "output/task2"
+    new Directory(new File(OUTPUT_DIRECTORY)).deleteRecursively()
 
     val spark = SparkSession.builder()
       .appName("Airline Twitter Analysis")
@@ -20,15 +26,13 @@ object Task2AirlineTweets {
       .option("inferSchema", "true")
       .csv(filePath)
 
-
     val cleanedData = data.withColumn("cleaned_text",
       regexp_replace(lower($"text"), "[^a-zA-Z\\s]", "")
     )
 
     val sentiments = Seq("positive", "negative", "neutral")
     sentiments.foreach { sentiment =>
-      println(s"Top 5 words for sentiment: $sentiment")
-      cleanedData.filter($"airline_sentiment" === sentiment)
+      val topWords = cleanedData.filter($"airline_sentiment" === sentiment)
         .select($"cleaned_text")
         .as[String]
         .flatMap(_.split("\\s+"))
@@ -37,7 +41,12 @@ object Task2AirlineTweets {
         .count()
         .orderBy(desc("count"))
         .limit(5)
-        .show()
+
+      val sentimentOutputPath = s"output/task2/$sentiment-top-words"
+      topWords.write
+        .option("header", "true")
+        .csv(sentimentOutputPath)
+      println(s"Saved top 5 words for sentiment '$sentiment' to $sentimentOutputPath")
     }
 
     val complaints = data
@@ -49,11 +58,12 @@ object Task2AirlineTweets {
       .filter($"rank" === 1)
       .select($"airline", $"negativereason", $"count")
 
-    println("Top complaint reason per airline:")
-    complaints.collect()
-    complaints.show()
+    val complaintsOutputPath = "output/task2/top-complaints"
+    complaints.write
+      .option("header", "true")
+      .csv(complaintsOutputPath)
+    println(s"Saved top complaint reasons to $complaintsOutputPath")
 
     spark.stop()
   }
 }
-
--- spark/src/main/scala/Task4GraphEdgeCounts.scala
+++ spark/src/main/scala/Task4GraphEdgeCounts.scala
@@ -2,9 +2,15 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
+import java.io.File
+import scala.reflect.io.Directory
+
 object Task4GraphEdgeCounts {
   def main(args: Array[String]): Unit = {
     Logger.getRootLogger.setLevel(Level.OFF)
+
+    val OUTPUT_DIRECTORY = "output/task4"
+    new Directory(new File(OUTPUT_DIRECTORY)).deleteRecursively()
 
     val spark = SparkSession.builder()
       .appName("GraphEdgeCounts")
@@ -36,11 +42,13 @@ object Task4GraphEdgeCounts {
     val top10Outgoing: Array[(String, Int)] = outgoingCounts
       .top(10)(Ordering[Int].on[(String, Int)](_._2))
 
-    println("Top 10 nodes with the most incoming edges:")
-    top10Incoming.foreach(println)
+    val top10IncomingRDD: RDD[(String, Int)] = sc.parallelize(top10Incoming)
+    top10IncomingRDD.saveAsTextFile("output/task4/top10_incoming")
+    println("Top 10 nodes with the most incoming edges saved to output/top10_incoming")
 
-    println("Top 10 nodes with the most outgoing edges:")
-    top10Outgoing.foreach(println)
+    val top10OutgoingRDD: RDD[(String, Int)] = sc.parallelize(top10Outgoing)
+    top10OutgoingRDD.saveAsTextFile("output/task4/top10_outgoing")
+    println("Top 10 nodes with the most outgoing edges saved to output/top10_outgoing")
 
     val degreeCounts: RDD[(String, Int)] = outgoingCounts.fullOuterJoin(incomingCounts)
       .mapValues {
@@ -52,11 +60,13 @@ object Task4GraphEdgeCounts {
 
     val mean: Double = degreeCounts.values.sum() / degreeCounts.count()
 
-    val filteredDegreeCount = degreeCounts.filter{ case (_, degree) => degree >= mean }.count()
+    val filteredDegreeRDD = degreeCounts.filter { case (_, degree) => degree >= mean }
+    val filteredCount = filteredDegreeRDD.count()
+    println(s"Nodes with degree greater than or equal to the average degree ($mean): $filteredCount")
 
-    println(s"Nodes with degree greater than or equal to the average degree ($mean): $filteredDegreeCount")
+    filteredDegreeRDD.saveAsTextFile("output/task4/nodes_greater_than_average")
+    println(s"Nodes saved to output/task4/nodes_greater_than_average")
 
     spark.stop()
   }
 }
-
